b8a40e03caa6cbf09eb8955a61c2c857e91a677e
biruk <birukanley2022@gmail.com>
2022-04-09 12:40:47 +0300

    adding use effect

--- src/UseStates.js
+++ src/UseStates.js
@@ -1,5 +1,7 @@
 // here I use use state to see increment and decremment in react js
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+// lets play with use state
 
 const UseStates = () => {
   // this one is trying to create use state whose current value is 0 and create a function that is used to upadate the varibale named as likedislilke
@@ -13,9 +15,37 @@ const UseStates = () => {
     SetNumber(likedislike - 1);
   };
 
+  // method two destructuring
   const add = () => {
-    SetNumber(likedislike + 1);
+    SetNumber((reaction) => reaction + 1);
   };
+
+  // this one is an interactive which is changed when a user clikes when post cliked
+  let [posts, setpost] = useState("nothing");
+
+
+
+  //this one will set up set time  out with use effect and also clean up effect availble
+
+  const[timee, settime]=useState(0)
+
+  // lets play with use effect
+  useEffect(() => {
+    document.title = ` clikce ${count} times`;
+    console.log("useeffect run");
+    console.log(count);
+
+    // run after every second
+   let timer= setTimeout(() => {   // the variable is used for clean up
+      settime((count) => count + 1);
+      
+    }, 1000);
+
+    // lets clean up
+    return () => clearTimeout(timer);
+
+
+  },); // this one specify which one will be affected ..dependency holder
 
   return (
     <div>
@@ -25,9 +55,16 @@ const UseStates = () => {
         <button onClick={add}>like</button>
       </div>
 
+      {/* let see when a user click the content changes */}
+      <button onClick={() => setpost("post clicked")}>Post</button>
+      <button onClick={() => setpost("commnet clicked")}>comment</button>
+      <h1>{posts}</h1>
+
       {/*this is method two calling set counter with it and passing the value */}
       <p>the button is clicke {count} times</p>
       <button onClick={() => Setcounter(count + 1)}>counter</button>
+
+      <h4>showing {timee}</h4>
     </div>
   );
 };
