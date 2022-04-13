353ca316cd06332bf395506a8f76210f79773976
biruk <birukanley2022@gmail.com>
2022-04-13 11:08:53 +0300

    multiple context with out hook

--- src/Usecontext1.js
+++ src/Usecontext1.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Usecontext2 from './Usecontext2';
 
 {/*  create a context and ppass value with export method */}
-export const Usercontext=React.createContext()
+export const Usercontext = React.createContext()
+export const detailcontext= React.createContext()
 function Usecontext1() {
     return (
-        <div>
-            <Usercontext.Provider value={"KIbur life"}>
-                <Usecontext2/>
-            </Usercontext.Provider>
-            
-        </div>
+      <div>
+        <Usercontext.Provider value={"KIbur life"}>
+          <detailcontext.Provider value={"detail for Kibur life which is nested"}>
+            <Usecontext2 />
+          </detailcontext.Provider>
+        </Usercontext.Provider>
+      </div>
     );
 }
 
--- src/UsecontextFinal.js
+++ src/UsecontextFinal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Usercontext,detailcontext } from './Usecontext1';
 
 
 function UsecontextFinal() {
@@ -6,6 +7,25 @@ function UsecontextFinal() {
         <div>
             <h2>Usecontext</h2>
             <p>this is the contetn which is Grand grand son of App.js after passing the 3 components</p>
+            <Usercontext.Consumer>
+
+                {
+                    username => {
+                        return (
+                            <detailcontext.Consumer>
+                                {
+                                    title => {
+                                        return (
+                                            <div className="user">
+                                                <h5>Passed value  using context is :::</h5><h1>{username} with the second detail of { title}</h1>
+                                            </div>
+                          );}}
+                            
+                          </detailcontext.Consumer>
+                        );
+                    }
+                }
+            </Usercontext.Consumer>
         </div>
     );
 }
